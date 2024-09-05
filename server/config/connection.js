7193a0c9045b002eae9ee948f8d82d0cd1db4442
Wanipopota <guanipag680@gmail.com>
2024-09-04 22:16:00 -0400

    solving conection to mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/googlebooks');
+connect(process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = connection;
